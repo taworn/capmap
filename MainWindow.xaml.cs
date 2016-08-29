2e6636734a539d007f14cfc1144bc6e5f33858d5
Taworn T <taworn@gmail.com>
2016-08-29 12:46:42 +0700

    - Added standard File menu.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,23 +1,70 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Win32;
+using System;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace capmap {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+
         public MainWindow() {
             InitializeComponent();
         }
+
+        private void menuFileNew(object sender, RoutedEventArgs e) {
+            var children = mapGrid.Children;
+            children.Clear();
+            for (int i = 0; i < 256; i++) {
+                var image = new Image();
+                BitmapImage bitmap = new BitmapImage();
+                bitmap.BeginInit();
+                bitmap.UriSource = new Uri("pack://application:,,,/capmap;component/res/block.png");
+                bitmap.EndInit();
+                image.Source = bitmap;
+                children.Add(image);
+            }
+        }
+
+        private void menuFileOpen(object sender, RoutedEventArgs e) {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.CheckFileExists = true;
+            dialog.CheckPathExists = true;
+            dialog.DefaultExt = ".map";
+            dialog.FileName = "";
+            dialog.Filter = "Map Files (*.map)|*.map|All Files (*.*)|*.*";
+            var result = dialog.ShowDialog();
+            if (result == true) {
+                //
+            }
+        }
+
+        private void menuFileSave(object sender, RoutedEventArgs e) {
+            //
+        }
+
+        private void menuFileSaveAs(object sender, RoutedEventArgs e) {
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.CheckPathExists = true;
+            dialog.DefaultExt = ".map";
+            dialog.FileName = "";
+            dialog.Filter = "Map Files (*.map)|*.map|All Files (*.*)|*.*";
+            var result = dialog.ShowDialog();
+            if (result == true) {
+                //
+            }
+        }
+
+        private void menuFileClose(object sender, RoutedEventArgs e) {
+            var children = mapGrid.Children;
+            children.Clear();
+        }
+
+        private void menuFileExit(object sender, RoutedEventArgs e) {
+            Close();
+        }
+
     }
 }
