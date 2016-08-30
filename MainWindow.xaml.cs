5e921441531091b56c653ecd2d49d8c76100dd02
Taworn T <taworn@gmail.com>
2016-08-30 11:05:02 +0700

    - Basic map editing.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -10,20 +10,24 @@ namespace capmap {
     /// </summary>
     public partial class MainWindow : Window {
 
+        private Map map = null;
+        private bool changed = false;
+        private string fileName = null;
+
+        private Image imageSource = null;
+
         public MainWindow() {
             InitializeComponent();
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e) {
+            radioBlock.IsChecked = true;
+
             var children = mapGrid.Children;
             children.Clear();
             for (int i = 0; i < 256; i++) {
-                var bitmap = new BitmapImage();
-                bitmap.BeginInit();
-                bitmap.UriSource = new Uri("pack://application:,,,/capmap;component/res/block.png");
-                bitmap.EndInit();
                 var image = new Image();
-                image.Source = bitmap;
+                image.Source = imageBlock.Source;
                 image.Margin = new Thickness(1, 1, 1, 1);
                 image.MouseDown += Image_MouseDown;
                 image.Tag = i;
@@ -33,6 +37,41 @@ namespace capmap {
 
         private void Window_Closed(object sender, EventArgs e) {
             //
+        }
+
+        private void Image_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e) {
+            var image = (Image)sender;
+            if (imageSource != null) {
+                image.Source = imageSource.Source;
+            }
+        }
+
+        private void RadioButton_Click(object sender, RoutedEventArgs e) {
+            var radio = (RadioButton)sender;
+            var tag = radio.Tag.ToString();
+
+            if (tag == "1") {
+                imageSource = imageBlock;
+            }
+            else if (tag == "2") {
+                imageSource = imageMovable;
+            }
+            else if (tag == "3") {
+                imageSource = imageCoke;
+            }
+            else if (tag == "4") {
+                imageSource = imageBread;
+            }
+            else if (tag == "5") {
+                imageSource = imageDivo;
+            }
+            else if (tag == "6") {
+                imageSource = imagePacman;
+            }
+            else {
+                imageSource = null;
+            }
+
         }
 
         private void menuFileNew(object sender, RoutedEventArgs e) {
@@ -80,6 +119,10 @@ namespace capmap {
             }
         }
 
+        private void menuFileExportToJavaScript(object sender, RoutedEventArgs e) {
+            //
+        }
+
         private void menuFileClose(object sender, RoutedEventArgs e) {
             /*
             var children = mapGrid.Children;
@@ -89,11 +132,6 @@ namespace capmap {
 
         private void menuFileExit(object sender, RoutedEventArgs e) {
             Close();
-        }
-
-        private void Image_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e) {
-            var image = (Image)sender;
-            MessageBox.Show(this, "Tag is " + image.Tag, "Info", MessageBoxButton.OK, MessageBoxImage.Information);
         }
 
     }
---
+++ Map.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace capmap {
+    public class Map {
+
+        private int width;
+        private int height;
+        private int[] blockMap;
+        private int[] imageMap;
+
+        /// <summary>
+        /// Constructs a map.
+        /// </summary>
+        public Map() {
+            width = 0;
+            height = 0;
+            blockMap = null;
+            imageMap = null;
+        }
+
+        /// <summary>
+        /// Opens a map file.
+        /// </summary>
+        /// <param name="fileName">Filename to open.</param>
+        public void open(string fileName) {
+
+        }
+
+        /// <summary>
+        /// Saves a map file.
+        /// </summary>
+        /// <param name="fileName">Filename to save.</param>
+        public void save(string fileName) {
+
+        }
+
+        public bool isOpened() {
+            return imageMap != null;
+        }
+
+    }
+}
