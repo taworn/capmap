4b9b7067392c66587aeed2bef5debea2c3408327
Taworn T <taworn@gmail.com>
2016-08-30 13:57:23 +0700

    - Added Map class. - Writes Map.Open() and Map.Save() functions.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -10,9 +10,11 @@ namespace capmap {
     /// </summary>
     public partial class MainWindow : Window {
 
-        private Map map = null;
-        private bool changed = false;
-        private string fileName = null;
+        private string title = null;
+
+        private Map mapObject = null;
+        private bool mapChanged = false;
+        private string mapFileName = null;
 
         private Image imageSource = null;
 
@@ -21,8 +23,71 @@ namespace capmap {
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e) {
+            title = Title;
             radioBlock.IsChecked = true;
+            imageSource = imageBlock;
+            EnableMenu(false);
+        }
 
+        private void Window_Closed(object sender, EventArgs e) {
+            //
+        }
+
+        private void Image_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e) {
+            var image = (Image)sender;
+            if (imageSource != null) {
+                int imageTag = int.Parse(image.Tag.ToString());
+                int imageSourceTag = int.Parse(imageSource.Tag.ToString());
+                mapObject.Set(imageTag, imageSourceTag);
+                image.Source = imageSource.Source;
+
+                if (!mapChanged) {
+                    mapChanged = true;
+                    Title = (mapChanged ? "* " : "") + (mapFileName != null ? mapFileName : "new map") + " - " + title;
+                }
+            }
+        }
+
+        private void RadioButton_Click(object sender, RoutedEventArgs e) {
+            var radio = (RadioButton)sender;
+            if (mapObject != null) {
+                var tag = radio.Tag.ToString();
+                if (tag == "1") {
+                    imageSource = imageBlock;
+                }
+                else if (tag == "2") {
+                    imageSource = imageMovable;
+                }
+                else if (tag == "3") {
+                    imageSource = imageCoke;
+                }
+                else if (tag == "4") {
+                    imageSource = imageBread;
+                }
+                else if (tag == "5") {
+                    imageSource = imageDivo;
+                }
+                else if (tag == "6") {
+                    imageSource = imagePacman;
+                }
+                else {
+                    imageSource = null;
+                }
+            }
+        }
+
+        private void menuFileNew_Click(object sender, RoutedEventArgs e) {
+            // if maps opened and its changed
+            if (mapObject != null && mapChanged) {
+                var r = MessageBox.Show(this, "Map data does not save.  Are you want to discard?", "Warning", MessageBoxButton.YesNo, MessageBoxImage.Exclamation);
+                if (r == MessageBoxResult.No)
+                    return;
+            }
+
+            // starts new map
+            mapObject = new Map(16, 16);
+            mapChanged = false;
+            mapFileName = null;
             var children = mapGrid.Children;
             children.Clear();
             for (int i = 0; i < 256; i++) {
@@ -33,64 +98,12 @@ namespace capmap {
                 image.Tag = i;
                 children.Add(image);
             }
+
+            Title = (mapChanged ? "* " : "") + (mapFileName != null ? mapFileName : "new map") + " - " + title;
+            EnableMenu(true);
         }
 
-        private void Window_Closed(object sender, EventArgs e) {
-            //
-        }
-
-        private void Image_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e) {
-            var image = (Image)sender;
-            if (imageSource != null) {
-                image.Source = imageSource.Source;
-            }
-        }
-
-        private void RadioButton_Click(object sender, RoutedEventArgs e) {
-            var radio = (RadioButton)sender;
-            var tag = radio.Tag.ToString();
-
-            if (tag == "1") {
-                imageSource = imageBlock;
-            }
-            else if (tag == "2") {
-                imageSource = imageMovable;
-            }
-            else if (tag == "3") {
-                imageSource = imageCoke;
-            }
-            else if (tag == "4") {
-                imageSource = imageBread;
-            }
-            else if (tag == "5") {
-                imageSource = imageDivo;
-            }
-            else if (tag == "6") {
-                imageSource = imagePacman;
-            }
-            else {
-                imageSource = null;
-            }
-
-        }
-
-        private void menuFileNew(object sender, RoutedEventArgs e) {
-            /*
-            var children = mapGrid.Children;
-            children.Clear();
-            for (int i = 0; i < 256; i++) {
-                var image = new Image();
-                BitmapImage bitmap = new BitmapImage();
-                bitmap.BeginInit();
-                bitmap.UriSource = new Uri("pack://application:,,,/capmap;component/res/block.png");
-                bitmap.EndInit();
-                image.Source = bitmap;
-                children.Add(image);
-            }
-            */
-        }
-
-        private void menuFileOpen(object sender, RoutedEventArgs e) {
+        private void menuFileOpen_Click(object sender, RoutedEventArgs e) {
             var dialog = new OpenFileDialog();
             dialog.CheckFileExists = true;
             dialog.CheckPathExists = true;
@@ -99,15 +112,45 @@ namespace capmap {
             dialog.Filter = "Map Files (*.map)|*.map|All Files (*.*)|*.*";
             var result = dialog.ShowDialog();
             if (result == true) {
-                //
+                // if maps opened and its changed
+                if (mapObject != null && mapChanged) {
+                    var r = MessageBox.Show(this, "Map data does not save.  Are you want to discard?", "Warning", MessageBoxButton.YesNo, MessageBoxImage.Exclamation);
+                    if (r == MessageBoxResult.No)
+                        return;
+                }
+
+                // starts new map
+                mapObject = new Map(16, 16);
+                mapObject.Open(dialog.FileName);
+                mapChanged = false;
+                mapFileName = dialog.FileName;
+                var children = mapGrid.Children;
+                children.Clear();
+                for (int i = 0; i < 256; i++) {
+                    var image = new Image();
+                    image.Source = MapDataToImage(mapObject.Get(i)).Source;
+                    image.Margin = new Thickness(1, 1, 1, 1);
+                    image.MouseDown += Image_MouseDown;
+                    image.Tag = i;
+                    children.Add(image);
+                }
+
+                Title = (mapChanged ? "* " : "") + (mapFileName != null ? mapFileName : "new map") + " - " + title;
+                EnableMenu(true);
             }
         }
 
-        private void menuFileSave(object sender, RoutedEventArgs e) {
-            //
+        private void menuFileSave_Click(object sender, RoutedEventArgs e) {
+            if (mapFileName != null) {
+                mapObject.Save(mapFileName);
+                mapChanged = false;
+            }
+            else {
+                menuFileSaveAs_Click(sender, e);
+            }
         }
 
-        private void menuFileSaveAs(object sender, RoutedEventArgs e) {
+        private void menuFileSaveAs_Click(object sender, RoutedEventArgs e) {
             var dialog = new SaveFileDialog();
             dialog.CheckPathExists = true;
             dialog.DefaultExt = ".map";
@@ -115,24 +158,58 @@ namespace capmap {
             dialog.Filter = "Map Files (*.map)|*.map|All Files (*.*)|*.*";
             var result = dialog.ShowDialog();
             if (result == true) {
-                //
+                mapFileName = dialog.FileName;
+                mapObject.Save(mapFileName);
+                mapChanged = false;
             }
         }
 
-        private void menuFileExportToJavaScript(object sender, RoutedEventArgs e) {
+        private void menuFileExportToJavaScript_Click(object sender, RoutedEventArgs e) {
             //
         }
 
-        private void menuFileClose(object sender, RoutedEventArgs e) {
-            /*
+        private void menuFileClose_Click(object sender, RoutedEventArgs e) {
+            // if maps opened and its changed
+            if (mapObject != null && mapChanged) {
+                var r = MessageBox.Show(this, "Map data does not save.  Are you want to discard?", "Warning", MessageBoxButton.YesNo, MessageBoxImage.Exclamation);
+                if (r == MessageBoxResult.No)
+                    return;
+            }
+
+            mapObject = null;
+            mapChanged = false;
+            mapFileName = null;
             var children = mapGrid.Children;
             children.Clear();
-            */
         }
 
-        private void menuFileExit(object sender, RoutedEventArgs e) {
+        private void menuFileExit_Click(object sender, RoutedEventArgs e) {
             Close();
         }
 
+        private void EnableMenu(bool enabled) {
+            menuFileSave.IsEnabled = enabled;
+            menuFileSaveAs.IsEnabled = enabled;
+            menuFileExportToJavaScript.IsEnabled = enabled;
+            menuFileClose.IsEnabled = enabled;
+        }
+
+        private Image MapDataToImage(int data) {
+            switch (data) {
+                default:
+                case 1:
+                    return imageBlock;
+                case 2:
+                    return imageMovable;
+                case 3:
+                    return imageCoke;
+                case 4:
+                    return imageBread;
+                case 5:
+                    return imageDivo;
+                case 6:
+                    return imagePacman;
+            }
+        }
     }
 }
--- Map.cs
+++ Map.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.IO;
 
 namespace capmap {
     public class Map {
@@ -12,31 +11,115 @@ namespace capmap {
         /// <summary>
         /// Constructs a map.
         /// </summary>
-        public Map() {
-            width = 0;
-            height = 0;
-            blockMap = null;
-            imageMap = null;
+        public Map(int w, int h) {
+            width = w;
+            height = h;
+            blockMap = new int[width * height];
+            imageMap = new int[width * height];
         }
 
         /// <summary>
         /// Opens a map file.
         /// </summary>
         /// <param name="fileName">Filename to open.</param>
-        public void open(string fileName) {
+        public void Open(string fileName) {
+            var stream = new FileStream(fileName, FileMode.Open);
+            var reader = new BinaryReader(stream);
+            try {
+                // reads header, 8 bytes
+                // header=CAPMAP##, # = ASCII 0
+                byte[] check = { 0x43, 0x41, 0x50, 0x4D, 0x41, 0x50, 0x00, 0x00, };
+                var header = reader.ReadBytes(8);
+                for (int i = 0; i < header.Length; i++)
+                    if (header[i] != check[i])
+                        throw new IOException("Header is not acceptable.");
 
+                // reads header information
+                // 6 int = width, height, divo start width, divo start height, pacman start width, pacman start height
+                int w = reader.ReadInt32();
+                int h = reader.ReadInt32();
+                if (w != 16 && h != 16)
+                    throw new IOException("Width and Height must be 16 for now.");
+                int dw = reader.ReadInt32();
+                int dh = reader.ReadInt32();
+                int pw = reader.ReadInt32();
+                int ph = reader.ReadInt32();
+
+                // reads block map and image map data
+                int size = width * height;
+                int[] blockMap = new int[size];
+                int[] imageMap = new int[size];
+                for (int i = 0; i < size; i++)
+                    blockMap[i] = reader.ReadInt32();
+                for (int i = 0; i < size; i++)
+                    imageMap[i] = reader.ReadInt32();
+
+                // copying data
+                this.width = w;
+                this.height = h;
+                //this. = dw;
+                //this. = dh;
+                //this. = pw;
+                //this. = ph;
+                this.blockMap = blockMap;
+                this.imageMap = imageMap;
+            }
+            finally {
+                reader.Close();
+                stream.Close();
+            }
         }
 
         /// <summary>
         /// Saves a map file.
         /// </summary>
         /// <param name="fileName">Filename to save.</param>
-        public void save(string fileName) {
+        public void Save(string fileName) {
+            var stream = new FileStream(fileName, FileMode.Create);
+            var writer = new BinaryWriter(stream);
+            try {
+                // writes header, 8 bytes
+                // header=CAPMAP##, # = ASCII 0
+                byte[] check = { 0x43, 0x41, 0x50, 0x4D, 0x41, 0x50, 0x00, 0x00, };
+                writer.Write(check);
 
+                // writes header information
+                // 6 int = width, height, divo start width, divo start height, pacman start width, pacman start height
+                writer.Write(width);
+                writer.Write(height);
+                writer.Write(0);
+                writer.Write(0);
+                writer.Write(0);
+                writer.Write(0);
+
+                // writes block map and image map data
+                int size = width * height;
+                for (int i = 0; i < size; i++)
+                    writer.Write(blockMap[i]);
+                for (int i = 0; i < size; i++)
+                    writer.Write(imageMap[i]);
+            }
+            finally {
+                writer.Close();
+                stream.Close();
+            }
         }
 
-        public bool isOpened() {
-            return imageMap != null;
+        public int GetWidth() {
+            return width;
+        }
+
+        public int GetHeight() {
+            return height;
+        }
+
+        public int Get(int index) {
+            return blockMap[index];
+        }
+
+        public void Set(int index, int data) {
+            blockMap[index] = data;
+            imageMap[index] = data;
         }
 
     }
